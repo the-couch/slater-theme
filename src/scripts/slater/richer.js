bb3f05503be48f166c53dcdc1e4aac312a5c488f
iamkevingreen <thedudem4n@gmail.com>
2017-08-22 20:58:52 -0400

    start to wrap the addToCart and serialize

--- src/scripts/slater/richer.js
+++ src/scripts/slater/richer.js
@@ -1,17 +1,44 @@
 // Handles all the things ajax cart related,
 // based around the timber ajax cart, minus the jquery
+import serialize from 'form-serialize'
 
+const RicherAPI = {}
+
+RicherAPI.addItemFromForm = (form, callback, errorCallback) => {
+  form = serialize(form, {hash: true})
+  console.log('serialized', form)
+}
 
 let Richer = {
   dom: {
     addToCart: document.querySelectorAll('.js-add-to-cart'),
+    addToCartForm: document.getElementById('AddToCartForm'),
     cartContainer: document.getElementById('CartContainer')
   },
   config: {
     items: []
   },
   init(options) {
-    console.log('cart', this)
+    this.dom.addToCartForm ? this.AddToCart() : null
+  },
+  AddToCart() {
+    const form = this.dom.addToCartForm
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      form.classList.remove('is-added')
+      form.classList.add('is-adding')
+
+      RicherAPI.addItemFromForm(e.target, itemAddedCallback, itemErrorCallback)
+    })
+
+    const itemAddedCallback = (product) => {
+      console.log('yo', product)
+    }
+
+    const itemErrorCallback = (XMLHttpRequest, textStatus) => {
+      console.log('error family')
+    }
   }
 }
 
